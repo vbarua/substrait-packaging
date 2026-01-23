5b70def77fa37af12a9158566438677233615db9
Victor Barua <victor.barua@datadoghq.com>
2026-01-23 15:08:25 -0800

    test: parser tests

--- python/substrait-antlr/tests/test_antlr.py
+++ python/substrait-antlr/tests/test_antlr.py
@@ -1,38 +1,50 @@
-def test_antlr_imports():
-    """Test that ANTLR generated parsers can be imported."""
-    from substrait_antlr.type_parser.SubstraitTypeLexer import SubstraitTypeLexer
-    from substrait_antlr.type_parser.SubstraitTypeParser import SubstraitTypeParser
-    from substrait_antlr.test_case_parser.FuncTestCaseLexer import FuncTestCaseLexer
-    from substrait_antlr.test_case_parser.FuncTestCaseParser import FuncTestCaseParser
 
-def test_type_parser_basic():
-    """Test basic functionality of the SubstraitType parser."""
-    from antlr4 import InputStream, CommonTokenStream
-    from substrait_antlr.type_parser.SubstraitTypeLexer import SubstraitTypeLexer
-    from substrait_antlr.type_parser.SubstraitTypeParser import SubstraitTypeParser
+from antlr4 import InputStream, CommonTokenStream
+from pathlib import Path
 
-    # Test parsing a simple type
-    input_stream = InputStream("i32")
-    lexer = SubstraitTypeLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = SubstraitTypeParser(stream)
+from substrait_antlr.substrait_type.SubstraitTypeLexer import SubstraitTypeLexer
+from substrait_antlr.substrait_type.SubstraitTypeParser import SubstraitTypeParser
+from substrait_antlr.substrait_type.SubstraitTypeVisitor import SubstraitTypeVisitor
+from substrait_antlr.func_test_case.FuncTestCaseLexer import FuncTestCaseLexer
+from substrait_antlr.func_test_case.FuncTestCaseParser import FuncTestCaseParser
+from substrait_antlr.func_test_case.FuncTestCaseParserVisitor import FuncTestCaseParserVisitor
 
-    # Parse the type - this should not raise an exception
-    tree = parser.scalarType()
-    assert tree is not None
+# Make sure that SubstraitTypeVisitor is available
+class SubstraitDummyVisitor(SubstraitTypeVisitor):
+    pass
 
-def test_test_case_parser_basic():
-    """Test basic functionality of the FuncTestCase parser."""
-    from antlr4 import InputStream, CommonTokenStream
-    from substrait_antlr.test_case_parser.FuncTestCaseLexer import FuncTestCaseLexer
-    from substrait_antlr.test_case_parser.FuncTestCaseParser import FuncTestCaseParser
+# Make sure that FuncTestCaseParserVisitor is available
+class FuncTestDummyVisitor(FuncTestCaseParserVisitor):
+    pass
 
-    # Test parsing a simple test case fragment
-    input_stream = InputStream("# comment\n")
-    lexer = FuncTestCaseLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = FuncTestCaseParser(stream)
+def test_type_parser_simple_type():
+    lexer = SubstraitTypeLexer(InputStream("i32"))
+    parser = SubstraitTypeParser(CommonTokenStream(lexer))
 
-    # Parse - this should not raise an exception
-    tree = parser.doc()
-    assert tree is not None
+    assert parser.typeDef().scalarType().toStringTree(recog=parser) == '(scalarType i32)'
+
+def test_type_parser_composite_type():
+    lexer = SubstraitTypeLexer(InputStream("list?<any1>>"))
+    parser = SubstraitTypeParser(CommonTokenStream(lexer))
+
+    assert parser.typeDef().parameterizedType().toStringTree(recog=parser) == '(parameterizedType list ? < (expr (typeDef (anyType any1))) >)'
+
+def test_scalar_test_parsing():
+    tc = load_test_case("test_scalar_test.yaml")
+    lexer = FuncTestCaseLexer(InputStream(tc))
+    parser = FuncTestCaseParser(CommonTokenStream(lexer))
+
+    assert parser.doc().header().version().toStringTree(recog=parser) == '(version ### SUBSTRAIT_SCALAR_TEST : v1.0)'
+
+def test_aggregate_test_parsing():
+    tc = load_test_case("test_aggregate_test.yaml")
+    lexer = FuncTestCaseLexer(InputStream(tc))
+    parser = FuncTestCaseParser(CommonTokenStream(lexer))
+
+    assert parser.doc().header().version().toStringTree(recog=parser) == '(version ### SUBSTRAIT_AGGREGATE_TEST : v1.0)'
+
+def load_test_case(testname):
+    """Load and return the content of antlr/test/<file>"""
+    test_file = Path(__file__).parent.parent.parent.parent / "antlr" / "tests" / testname
+    with open(test_file, "r") as f:
+        return f.read()
