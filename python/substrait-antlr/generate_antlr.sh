35829274a87c1df5923b7ee7eed66b77666a7810
Victor Barua <victor.barua@datadoghq.com>
2026-01-23 09:46:25 -0800

    ci: use vendored ANTLR

--- python/substrait-antlr/generate_antlr.sh
+++ python/substrait-antlr/generate_antlr.sh
@@ -4,20 +4,24 @@ set -eu
 
 SUBSTRAIT_HOME="${SUBSTRAIT_HOME:-../../substrait}"
 GRAMMAR_DIR="$SUBSTRAIT_HOME/grammar"
+ANTLR_PATH="${ANTLR_PATH:-../../antlr}"
 
 echo "Generating Python ANTLR files from $GRAMMAR_DIR"
 
+TYPES_TARGET="src/substrait_antlr/type_parser"
+FTEST_TARGET="src/substrait_antlr/test_case_parser"
+
 # Clean target directories
-rm -rf src/substrait_antlr/type_parser/*
-rm -rf src/substrait_antlr/test_case_parser/*
+rm -rf "$TYPES_TARGET/*"
+rm -rf "$FTEST_TARGET/*"
 
 # Generate SubstraitType parser
 echo "Generating SubstraitType parser..."
-antlr4 -o src/substrait_antlr/type_parser -Dlanguage=Python3 "$GRAMMAR_DIR/SubstraitType.g4"
+. "$ANTLR_PATH/antlr.sh" -o $TYPES_TARGET -Dlanguage=Python3 "$GRAMMAR_DIR/SubstraitType.g4"
 
 # Generate FuncTestCase parser
 echo "Generating FuncTestCase parser..."
-antlr4 -o src/substrait_antlr/test_case_parser -Dlanguage=Python3 "$GRAMMAR_DIR/FuncTestCaseParser.g4"
+. "$ANTLR_PATH/antlr.sh" -o $FTEST_TARGET -Dlanguage=Python3 "$GRAMMAR_DIR/FuncTestCaseParser.g4"
 
 # Clean up .tokens and .interp files
 echo "Cleaning up intermediate files..."
